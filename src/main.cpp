81ff8ae380f186d2d4490d5698b38b9a87d1da20
michaelsalton <msalton6@gmail.com>
2025-07-12 22:40:21 -0400

    fire shader working

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,10 @@ int main()
     const unsigned int WINDOW_WIDTH = 800;
     const unsigned int WINDOW_HEIGHT = 600;
     
-    sf::RenderWindow window(sf::VideoMode(WINDOW_WIDTH, WINDOW_HEIGHT), "Fire Shader");
+    sf::RenderWindow window(sf::VideoMode({WINDOW_WIDTH, WINDOW_HEIGHT}), "Fire Shader");
     window.setFramerateLimit(60);
     
-    sf::RectangleShape fullscreenQuad(sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+    sf::RectangleShape fullscreenQuad(sf::Vector2f(static_cast<float>(WINDOW_WIDTH), static_cast<float>(WINDOW_HEIGHT)));
     fullscreenQuad.setFillColor(sf::Color::White);
     
     sf::Shader fireShader;
@@ -21,16 +21,15 @@ int main()
     sf::Clock clock;
     
     while (window.isOpen()) {
-        sf::Event event;
-        while (window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed) {
+        while (const auto event = window.pollEvent()) {
+            if (event->is<sf::Event::Closed>()) {
                 window.close();
             }
         }
         
         float time = clock.getElapsedTime().asSeconds();
         fireShader.setUniform("time", time);
-        fireShader.setUniform("resolution", sf::Vector2f(WINDOW_WIDTH, WINDOW_HEIGHT));
+        fireShader.setUniform("resolution", sf::Vector2f(static_cast<float>(WINDOW_WIDTH), static_cast<float>(WINDOW_HEIGHT)));
         
         window.clear();
         window.draw(fullscreenQuad, &fireShader);
